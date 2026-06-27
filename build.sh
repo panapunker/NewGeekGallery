d574a5c041aa648693630cf49a3b31f19c6284e2
panapunker <ospisebas89@gmail.com>
2026-06-27 13:31:36 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,3 +7,7 @@ pip install -r requirements.txt
 python manage.py collectstatic --noinput
 
 python manage.py migrate
+
+if [[ $CREATE_SUPERUSER ]]; then
+    python manage.py createsuperuser --noinput || true
+fi
